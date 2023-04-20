510db02ece9de42b9f63affeef7920812599248a
Daniel Rojas <daniel.rojas.artiaga@gmail.com>
2023-04-19 23:38:27 -0600

    Initial commit

---
+++ src/test/java/com/fundamentosspring/springjava/SpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosspring.springjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
